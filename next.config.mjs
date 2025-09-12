eef242a3de30476eff0bb096ecc6f09f8ce36ea0
yashraj5656 <yashrajsingh3876@gmail.com>
2025-09-12 17:10:16 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      // ✅ Ignore ESLint errors during builds (Netlify/Vercel/etc.)
+      ignoreDuringBuilds: true,
+    },
+    webpack: (config) => {
+      config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
+        stackframe: false, // ⛔ tell Next.js to ignore "stackframe"
+      };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
